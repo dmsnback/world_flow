b4af64587fd5452d6f5cca4efb1a1674713b1cc2
Dmitry Titenkov <lt200711@yandex.ru>
2026-02-20 15:08:21 +0300

    модель слов, миграции

---
+++ app/models/words.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy import String, Integer, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.database import Base
+
+
+class Word(Base):
+    __tablename__ = "words"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    english: Mapped[str] = mapped_column(String(255), nullable=False)
+    russian: Mapped[str] = mapped_column(String(255), nullable=False)
+    create_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
